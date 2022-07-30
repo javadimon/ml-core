1cc671f2eb0887be070a7da802228f6a82dc30e7
Dmitry Zubanoff <zubanov@gmail.com>
2022-07-30 17:36:55 +0300

    Init repo

---
+++ src/test/java/com/zubanoff/ml/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubanoff.ml.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
